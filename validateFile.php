2c9e0be9295e40a2495c407dfe9393303e19ac19
EversonFeltrin <efeltrin@inf.ufsm.br>
2020-01-23 02:53:38 -0300

    Estruturando o ValidateFile.php em funções

--- validateFile.php
+++ validateFile.php
@@ -5,39 +5,18 @@
 
 	
 
-	//Função que verifica se a extensão do arquivo é .java
-	function checkExtensionFile($name_file){
-		//verifica a extensão do arquivo
-		$file_extension = substr($name_file, -4);
-
-		if (strcmp($file_extension, 'java') != 0){
-			return header('Location: index.php?codigo=1');
-		}
-		return 0;
-	}
-
-	//Função que verifica se o arquivo possui conteudo
-	function checkContentFile($name_file){
-		$file = file($name_file);
-		
-		//verifica se o arquivo tem conteudo para analisar
-		if (empty($file)){
-			return header('Location: index.php?codigo=2');
-		}
-		else {
-			return $file;
-		}
-
-	}
+	
 
 
 	checkExtensionFile($name_file);
 
 	$file = checkContentFile($name_file);
 
-		
-		//$num_linhas = count($arq);
-		
+	lineCounter($file);
+
+
+	function lineCounter($file){
+
 		// variavel para guardar o indice do array com o conteudo das linhas
 		$index = 0;
 		
@@ -52,31 +31,23 @@
 		
 		// Variavel para armazenar o número de linhas descartáveis
 		$count_lines = 0;
-
-
-
+	
 		foreach($file as $index => $content){
 			//$conteudo_linha[$i] =  $conteudo;
 
 			echo '<br> Linha: ' . $index . ' Conteúdo: '. $content . '<br>'; // . '<br>Substring:' . substr($conteudo, 0); 
 		}
-		//
+
+		// Percorre o arquivo e aribui o conteudo de cada linha a um array $content
 		foreach($file as $index => $content)
 		{
-			/*
-			if ($ini_coment_pos == 0 && $fim_coment_pos == 0)
-				if ($bloqueador == 0 && stripos($conteudo, '//') > -1){
-					$position = stripos(ltrim($conteudo), '//');
-					if ($position == 0){
-						//echo '<br>' .$i . ' | ' . $position . '<br>';
-					//echo '<br>Comentário Simples.' . stripos($conteudo, '//');
-					$count_linhas++;}
-				}
-			*/
-			// Verifica se a contagem de linhas para comentários de n linhas está iniciada		
+			// Verifica se a contagem de linhas para comentários de n linhas não está ativa		
 			if($ini_n_lines_coments == 0 && $end_n_lines_coments == 0)
+
 				//verifica se as linhas estão desbloqueadas para buscar comentário de uma linha
+				
 				//ltrim elimina os espaços a esquerda fazendo com que comentário de uma linha sempre inicie na posição 0 da linha
+				
 				if($blocker == 0 && stripos($content, '//') > -1)
 				{
 					$position = stripos(ltrim($content), '//');
@@ -122,6 +93,39 @@
 
 		}
 		echo '<br>' . $count_lines;
+	}
+
+
+
+	//Função que verifica se o arquivo possui conteudo
+	function checkContentFile($name_file){
+		$file = file($name_file);
+		
+		//verifica se o arquivo tem conteudo para analisar
+		if (empty($file)){
+			return header('Location: index.php?codigo=2');
+		}
+		else {
+			return $file;
+		}
+
+	}	
+	
+	//Função que verifica se a extensão do arquivo é .java
+	function checkExtensionFile($name_file){
+		//verifica a extensão do arquivo
+		$file_extension = substr($name_file, -4);
+
+		if (strcmp($file_extension, 'java') != 0){
+			return header('Location: index.php?codigo=1');
+		}
+		return 0;
+	}		
+		
+
+
+
+		
 		
 	
 	
